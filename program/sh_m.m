fce7f22450ee59c6703880f1e774dab29d6e7996
D1ver <robo_work@bk.ru>
2017-11-27 05:47:43 +0200

    1.0.4.1 // Добавлен функционал для изменения поля ввода по клику на список макросов

--- program/sh_m.m
+++ program/sh_m.m
@@ -19,7 +19,8 @@ out = uitable(...
 
 % многострочный текст в input
 set(input,'max',2);
-
+set(input,'FontSize',30);
+set(input,'HorizontalAlignment', 'left');
 % начальные данные на ленте (инициализация)
 set(out,'data',zeros(1,100));
 
@@ -29,7 +30,21 @@ set(out,'data',zeros(1,100));
 		[list, macros_list] = commandlist_creator(cellstr(cmb));
 		listarr = mdpi(list, macros_list);
 		listarr
+	end
+	
+	% функция определения позиции каретки
+	% TODO: написать поле с макросами, привязать
+	% функцию к каждому элементу этого поля, дабы ф-я
+	% принимала на вход имя элемента(макроса), выполнять по
+	% клику на элемент поля
+	function paste_macros (~,~)
 		jEditbox = findjobj(input);
-		get(jEditbox,'CaretPosition')
+		caretPos = get(jEditbox,'CaretPosition');
+		jEditbox = handle(jEditbox.getViewport.getView, 'CallbackProperties');
+		text = char(jEditbox.getText());
+		macrosName = ['\n',macrosName,'\n']; 
+		text = [text(1:caretPos), macrosName, text(caretPos:end)];
+		% вставить текст в определенное место программы - уже не проблема
+		jEditbox.setText(text);
 	end
 end
